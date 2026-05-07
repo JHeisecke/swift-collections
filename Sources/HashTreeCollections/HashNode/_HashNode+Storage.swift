3d463b4848ab78dcb7f30f458be7ef84d30eb359
Javier Heisecke <javier.heisecke@consultr.net>
2026-05-07 11:47:24 -0300

    fix: remove @inlinable from deinit in _HashNode.Storage for Swift 6.2

--- Sources/HashTreeCollections/HashNode/_HashNode+Storage.swift
+++ Sources/HashTreeCollections/HashNode/_HashNode+Storage.swift
@@ -44,7 +44,6 @@ extension _HashNode {
     @usableFromInline
     internal typealias UnsafeHandle = _HashNode<Key, Value>.UnsafeHandle
 
-    @inlinable
     deinit {
       UnsafeHandle.update(self) { handle in
         handle.children.deinitialize()
